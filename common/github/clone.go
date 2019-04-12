06b10ec89d6074dc21806bf82dd201a93f48ae31
Asgeir Nilsen <asgeir@fintlabs.no>
2019-04-12 09:39:35 +0200

    Added support for choosing a specific tag or branch of skeleton.

--- common/github/clone.go
+++ common/github/clone.go
@@ -1,18 +1,24 @@
 package github
 
 import (
+	"errors"
 	"fmt"
 	"os"
-	"gopkg.in/src-d/go-git.v4"
-	"errors"
+
 	"github.com/FINTLabs/fint-consumer/common/utils"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 )
 
-func Clone(name string, cloneUrl string) error  {
+func Clone(name string, cloneUrl string, reference string) error {
+
+	ref := plumbing.ReferenceName(reference)
 
 	r, err := git.PlainClone(utils.GetWorkingDir(name), false, &git.CloneOptions{
-		URL:      cloneUrl,
-		Progress: os.Stdout,
+		URL:           cloneUrl,
+		Progress:      os.Stdout,
+		Depth:         1,
+		ReferenceName: ref,
 	})
 
 	_, err = r.Head()
