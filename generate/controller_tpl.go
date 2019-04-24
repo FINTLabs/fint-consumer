2986b80431164eb8550c254ed0602919e9d41e16
Asgeir Nilsen <asgeir@fintlabs.no>
2019-04-24 13:14:46 +0200

    Handle event responses using utility class.

--- generate/controller_tpl.go
+++ generate/controller_tpl.go
@@ -16,6 +16,7 @@ import no.fint.consumer.event.ConsumerEventUtil;
 import no.fint.consumer.event.SynchronousEvents;
 import no.fint.consumer.exceptions.*;
 import no.fint.consumer.status.StatusCache;
+import no.fint.consumer.utils.EventResponses;
 import no.fint.consumer.utils.RestEndpoints;
 
 import no.fint.event.model.*;
@@ -159,15 +160,14 @@ public class {{ .Name }}Controller {
             BlockingQueue<Event> queue = synchronousEvents.register(event);
             consumerEventUtil.send(event);
 
-            Event response = queue.poll(5, TimeUnit.MINUTES);
+            Event response = EventResponses.handle(queue.poll(5, TimeUnit.MINUTES));
 
-            if (response == null ||
-                    response.getData() == null ||
+            if (response.getData() == null ||
                     response.getData().isEmpty()) throw new EntityNotFoundException(id);
 
             {{ $.Name }}Resource {{ ToLower $.Name }} = objectMapper.convertValue(response.getData().get(0), {{ $.Name }}Resource.class);
 
-            fintAuditService.audit(event, Status.SENT_TO_CLIENT);
+            fintAuditService.audit(response, Status.SENT_TO_CLIENT);
 
             return linker.toResource({{ ToLower $.Name }});
         }    
@@ -275,6 +275,11 @@ public class {{ .Name }}Controller {
     //
     // Exception handlers
     //
+    @ExceptionHandler(EventResponseException.class)
+    public ResponseEntity handleEventResponseException(EventResponseException e) {
+        return ResponseEntity.status(e.getStatus()).body(e.getResponse());
+    }
+
     @ExceptionHandler(UpdateEntityMismatchException.class)
     public ResponseEntity handleUpdateEntityMismatch(Exception e) {
         return ResponseEntity.badRequest().body(ErrorResponse.of(e));
