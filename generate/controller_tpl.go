3481c2e322d0c505fe2d4fb9044723edebd3d6f9
Asgeir Nilsen <asgeir@fintlabs.no>
2019-06-25 16:09:08 +0200

    Remove the /cache/rebuild endpoint.
    This belongs under /admin, and should be restricted.

--- generate/controller_tpl.go
+++ generate/controller_tpl.go
@@ -86,14 +86,6 @@ public class {{ .Name }}Controller {
         return ImmutableMap.of("size", cacheService.getAll(orgId).size());
     }
 
-    @PostMapping("/cache/rebuild")
-    public void rebuildCache(@RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId) {
-        if (props.isOverrideOrgId() || orgId == null) {
-            orgId = props.getDefaultOrgId();
-        }
-        cacheService.rebuildCache(orgId);
-    }
-
     @GetMapping
     public {{ .Name }}Resources get{{ .Name }}(
             @RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId,
